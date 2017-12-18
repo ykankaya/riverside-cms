5e16a235acedb9a37c6ac7e596e0facd3d31e059
Mike Puddephat <mikepuddephat@hotmail.com>
2017-12-18 18:31:51 +0000

    Implement read master page method of master pages API

---
+++ src/Services/Core/Core.Infrastructure/SqlMasterPageRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Options;
+using Riverside.Cms.Services.Core.Domain;
+
+namespace Riverside.Cms.Services.Core.Infrastructure
+{
+    public class SqlMasterPageRepository : IMasterPageRepository
+    {
+        private readonly IOptions<SqlOptions> _options;
+
+        public SqlMasterPageRepository(IOptions<SqlOptions> options)
+        {
+            _options = options;
+        }
+
+        public async Task<MasterPage> ReadMasterPageAsync(long tenantId, long masterPageId)
+        {
+            using (SqlConnection connection = new SqlConnection(_options.Value.SqlConnectionString))
+            {
+                connection.Open();
+
+                MasterPage masterPage = await connection.QueryFirstOrDefaultAsync<MasterPage>(
+                    @"SELECT TenantId, MasterPageId, Name, PageName, PageDescription, AncestorPageId, AncestorPageLevel, PageType, HasOccurred, HasImage,
+                        ThumbnailImageWidth, ThumbnailImageHeight, ThumbnailImageResizeMode, PreviewImageWidth, PreviewImageHeight, PreviewImageResizeMode, ImageMinWidth, ImageMinHeight,
+	                    Creatable, Deletable, Taggable, Administration, BeginRender, EndRender
+                        FROM cms.MasterPage WHERE TenantId = @TenantId AND MasterPageId = @MasterPageId",
+                    new { TenantId = tenantId, MasterPageId = masterPageId }
+                );
+
+                return masterPage;
+            }
+        }
+    }
+}
